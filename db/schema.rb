9d3128ddb4f5b79ff32aeefa5f4c51ce28fdfdbf
Sergey Shapovalov <ssv@fozzy.com>
2024-06-22 01:55:49 +0200

    Added repositories model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_14_135325) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_21_235419) do
+  create_table "repositories", force: :cascade do |t|
+    t.string "name"
+    t.string "github_id", null: false
+    t.string "full_name"
+    t.string "language"
+    t.string "clone_url"
+    t.string "ssh_url"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["github_id"], name: "index_repositories_on_github_id", unique: true
+    t.index ["user_id"], name: "index_repositories_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
     t.string "nickname"
@@ -19,4 +33,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_14_135325) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "repositories", "users"
 end
