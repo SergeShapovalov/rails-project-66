64e34956449066544111771ce37646c0a97fa169
Sergey Shapovalov <ssv@fozzy.com>
2024-06-23 11:32:36 +0200

    Added the ability to add a repository

--- app/controllers/web/repositories_controller.rb
+++ app/controllers/web/repositories_controller.rb
@@ -6,10 +6,28 @@ class Web::RepositoriesController < Web::ApplicationController
   def show; end
 
   def new
-    client = Octokit::Client.new(access_token: current_user.token)
-    @repositories = client.repositories(current_user.nickname)
-                          .filter { |repository| repository[:language].downcase == 'ruby' }
+    @repositories = repositories
   end
 
-  def create; end
+  def create
+    @repository = current_user.repositories.find_or_initialize_by(repository_params)
+
+    if @repository.save!
+      redirect_to repositories_path, notice: t('create_success')
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def repository_params
+    params.require(:repository).permit(:github_id)
+  end
+
+  def repositories
+    client = Octokit::Client.new(access_token: current_user.token)
+    client.repositories(current_user.nickname)
+          .filter { |repository| repository[:language].downcase == 'ruby' }
+  end
 end
