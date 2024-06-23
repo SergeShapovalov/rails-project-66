709793224fe328be4e4c49ec0d18c74729c03b11
Sergey Shapovalov <ssv@fozzy.com>
2024-06-23 22:03:44 +0200

    Added repository check model

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -2,6 +2,9 @@ class Repository < ApplicationRecord
   extend Enumerize
 
   belongs_to :user
+  has_many :checks, class_name: 'Repository::Check'
 
   enumerize :language, in: %w[ruby]
+
+  validates :github_id, uniqueness: true, presence: true
 end
