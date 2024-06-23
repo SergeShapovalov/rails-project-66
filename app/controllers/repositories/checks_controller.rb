fe17ef9794162a4e6945e08f13db8dcb14441b82
Sergey Shapovalov <ssv@fozzy.com>
2024-06-23 22:23:19 +0200

    Moved checks controller to web scope

--- app/controllers/repositories/checks_controller.rb
+++
@@ -1,5 +0,0 @@
-class Repositories::ChecksController < ApplicationController
-  def show; end
-
-  def create; end
-end
---
+++ app/controllers/web/repositories/checks_controller.rb
@@ -0,0 +1,5 @@
+class Repositories::ChecksController < Web::Repositories::ApplicationController
+  def show; end
+
+  def create; end
+end
