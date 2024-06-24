2e6a9e6e8e70a3c892d1178dbb9bf4a9bb16857b
Sergey Shapovalov <ssv@fozzy.com>
2024-06-24 23:32:46 +0200

    Added checks info to view

--- app/helpers/repositories/checks_helper.rb
+++ app/helpers/repositories/checks_helper.rb
@@ -1,2 +1,10 @@
 module Repositories::ChecksHelper
+  def github_url(check, options = {})
+    commit = check.commit_id.slice(..6)
+    file_path = options.delete(:file_path)
+    link_name = file_path.nil? ? commit : file_path
+    commit_path = file_path.nil? ? "/commit/#{commit}" : "/tree/#{commit}/#{file_path}"
+
+    link_to link_name, "https://github.com/#{check.repository.full_name}#{commit_path}", options
+  end
 end
