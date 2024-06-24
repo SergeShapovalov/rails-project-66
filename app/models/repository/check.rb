703e7db0cbac9be7fc1fc8a5ffc65092d15f33e7
Sergey Shapovalov <ssv@fozzy.com>
2024-06-24 21:07:55 +0200

    Added aasm states to repository check

--- app/models/repository/check.rb
+++ app/models/repository/check.rb
@@ -1,3 +1,22 @@
 class Repository::Check < ApplicationRecord
+  include AASM
+
   belongs_to :repository
+
+  aasm do
+    state :created, initial: true
+    state :requested, :finished, :failed
+
+    event :run_check do
+      transitions from: :created, to: :requested
+    end
+
+    event :finish do
+      transitions from: :requested, to: :finished
+    end
+
+    event :fail do
+      transitions from: :requested, to: :failed
+    end
+  end
 end
