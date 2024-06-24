bbccce2e744e9777ebdbe28aae0291c291ae8818
Sergey Shapovalov <ssv@fozzy.com>
2024-06-25 00:20:45 +0200

    Added first tests

--- test/controllers/web/repositories_controller_test.rb
+++ test/controllers/web/repositories_controller_test.rb
@@ -1,23 +1,19 @@
 require "test_helper"
 
 class Web::RepositoriesControllerTest < ActionDispatch::IntegrationTest
-  # test "should get index" do
-  #   get web_repositories_index_url
-  #   assert_response :success
-  # end
-  #
-  # test "should get show" do
-  #   get web_repositories_show_url
-  #   assert_response :success
-  # end
-  #
-  # test "should get new" do
-  #   get web_repositories_new_url
-  #   assert_response :success
-  # end
-  #
-  # test "should get create" do
-  #   get web_repositories_create_url
-  #   assert_response :success
-  # end
+  setup do
+    @user = users(:one)
+  end
+
+  test 'should get index page' do
+    sign_in @user
+
+    get repositories_url
+    assert :success
+  end
+
+  test 'should NOT get index page' do
+    get repositories_url
+    assert_redirected_to root_path
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,3 +13,31 @@ module ActiveSupport
     # Add more helper methods to be used by all tests here...
   end
 end
+
+module ActionDispatch
+  class IntegrationTest
+    def sign_in(user)
+      auth_hash = {
+        provider: 'github',
+        uid: '12345',
+        info: {
+          email: user.email,
+          nickname: user.nickname
+        },
+        credentials: { token: user.token }
+      }
+
+      OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash::InfoHash.new(auth_hash)
+
+      get callback_auth_url('github')
+    end
+
+    def signed_in?
+      session[:user_id].present? && current_user.present?
+    end
+
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+  end
+end
