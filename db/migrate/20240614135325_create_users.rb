69c0becf34c0296da15da134c3da53c94640f5f4
Sergey Shapovalov <ssv@fozzy.com>
2024-06-27 15:02:26 +0200

    Removed trash from migrate user

--- db/migrate/20240614135325_create_users.rb
+++ db/migrate/20240614135325_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[7.1]
   def change
     create_table :users do |t|
-      t.string :email, precision: true, null: false
+      t.string :email, null: false
       t.string :nickname
       t.string :token
 
